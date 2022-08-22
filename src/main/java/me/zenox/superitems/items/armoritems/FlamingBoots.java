ca2cb36f835f9b67a308998a55cfafd60382d984
ZenoCoding <51522505+ZenoCoding@users.noreply.github.com>
2022-08-21 18:22:54 -0700

    Fix Flaming + Void Boots andy u r stupid af

--- src/main/java/me/zenox/superitems/items/armoritems/FlamingBoots.java
+++ src/main/java/me/zenox/superitems/items/armoritems/FlamingBoots.java
@@ -20,53 +20,30 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-<<<<<<<< HEAD:src/main/java/me/zenox/superitems/items/armoritems/VoidBoots.java
-import static me.zenox.superitems.items.ItemRegistry.DESECRATOR_SCALE;
-import static me.zenox.superitems.items.ItemRegistry.DESECRATOR_TOE;
 
-public class VoidBoots extends ArmorItem {
-    public VoidBoots() {
-        super("Boots Of the Void Lord", "void_boots", Rarity.EPIC, Type.BOOTS, Material.LEATHER_BOOTS, Map.of(Stats.STRENGTH, 2d, Stats.HEALTH, 5d, Stats.WISDOM, 30d, Stats.REGENERATION, 5d));
-
-        List<String> lore = new ArrayList<>();
-        lore.add(ChatColor.GRAY + "Who the heck is this \"Void Lord\" guy?");
-========
 public class FlamingBoots extends ArmorItem {
     public FlamingBoots() {
         super("Flaming Boots", "flaming_boots", Rarity.EPIC, Type.BOOTS, Material.LEATHER_BOOTS, Map.of(Stats.STRENGTH, 8d, Stats.HEALTH, 5d));
 
         List<String> lore = new ArrayList<>();
         lore.add(ChatColor.GRAY + "Raw power.");
->>>>>>>> d5fd419ea4f0063f3bc088d1cf9b580aa446e384:src/main/java/me/zenox/superitems/items/armoritems/FlamingBoots.java
         this.getMeta().setLore(lore);
         ((LeatherArmorMeta) this.getMeta()).setColor(Color.ORANGE);
 
         Multimap<Attribute, AttributeModifier> attributeMap = ArrayListMultimap.create();
-<<<<<<<< HEAD:src/main/java/me/zenox/superitems/items/armoritems/VoidBoots.java
-        attributeMap.put(Attribute.GENERIC_ARMOR, new AttributeModifier(UUID.randomUUID(), "superitems:armor", 4, AttributeModifier.Operation.ADD_NUMBER, EquipmentSlot.FEET));
-        attributeMap.put(Attribute.GENERIC_ARMOR_TOUGHNESS, new AttributeModifier(UUID.randomUUID(), "superitems:armor_toughness", 2, AttributeModifier.Operation.ADD_NUMBER, EquipmentSlot.FEET));
 
-
-========
         attributeMap.put(Attribute.GENERIC_ARMOR, new AttributeModifier(UUID.randomUUID(), "superitems:armor", 2, AttributeModifier.Operation.ADD_NUMBER, EquipmentSlot.FEET));
         attributeMap.put(Attribute.GENERIC_ARMOR_TOUGHNESS, new AttributeModifier(UUID.randomUUID(), "superitems:armor_toughness", 10, AttributeModifier.Operation.ADD_NUMBER, EquipmentSlot.FEET));
         attributeMap.put(Attribute.GENERIC_ATTACK_SPEED, new AttributeModifier(UUID.randomUUID(), "superitems:attack_speed", 0.05, AttributeModifier.Operation.MULTIPLY_SCALAR_1, EquipmentSlot.FEET));
         attributeMap.put(Attribute.GENERIC_MOVEMENT_SPEED, new AttributeModifier(UUID.randomUUID(), "superitems:movement_speed", 0.05, AttributeModifier.Operation.MULTIPLY_SCALAR_1, EquipmentSlot.FEET));
->>>>>>>> d5fd419ea4f0063f3bc088d1cf9b580aa446e384:src/main/java/me/zenox/superitems/items/armoritems/FlamingBoots.java
+
         this.getMeta().setAttributeModifiers(attributeMap);
         this.getMeta().setUnbreakable(true);
     }
 
     @Override
     public List<Recipe> getRecipes() {
-<<<<<<<< HEAD:src/main/java/me/zenox/superitems/items/armoritems/VoidBoots.java
-//        ShapedRecipe recipe = new ShapedRecipe(this.getKey(), this.getItemStack(1));
-//        recipe.shape("   ", "S S", "T T");
-//        recipe.setIngredient('T', new RecipeChoice.ExactChoice(DESECRATOR_TOE.getItemStack(1)));
-//        recipe.setIngredient('S', new RecipeChoice.ExactChoice(DESECRATOR_SCALE.getItemStack(1)));
-========
         ShapedRecipe recipe = new ShapedRecipe(this.getKey(), this.getItemStack(1));
->>>>>>>> d5fd419ea4f0063f3bc088d1cf9b580aa446e384:src/main/java/me/zenox/superitems/items/armoritems/FlamingBoots.java
         return List.of();
     }
 }
