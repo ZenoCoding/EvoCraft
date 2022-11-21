8c4a3aa567851d4af05dd41812eaf9a345b6a57b
ZenoCoding <51522505+ZenoCoding@users.noreply.github.com>
2022-11-20 23:10:11 -0800

    Add missing attributes

--- src/main/java/me/zenox/superitems/attribute/AttributeRegistry.java
+++ src/main/java/me/zenox/superitems/attribute/AttributeRegistry.java
@@ -11,6 +11,9 @@ public class AttributeRegistry {
     public static final Attribute KNOCKBACK_RESISTANCE = new MinecraftAttribute("mc-knockback_resistance", ChatColor.DARK_GRAY, org.bukkit.attribute.Attribute.GENERIC_KNOCKBACK_RESISTANCE);
     public static final Attribute MOVEMENT_SPEED = new MinecraftAttribute("mc-movement_speed", ChatColor.WHITE, org.bukkit.attribute.Attribute.GENERIC_MOVEMENT_SPEED);
     public static final Attribute FLIGHT_SPEED = new MinecraftAttribute("mc-flight_speed", ChatColor.WHITE, org.bukkit.attribute.Attribute.GENERIC_FLYING_SPEED);
+    public static final Attribute ATTACK_DAMAGE = new MinecraftAttribute("mc-attack_damage", ChatColor.RED, org.bukkit.attribute.Attribute.GENERIC_ATTACK_DAMAGE);
+    public static final Attribute ATTACK_KNOCKBACK = new MinecraftAttribute("mc-attack_knockback", ChatColor.AQUA, org.bukkit.attribute.Attribute.GENERIC_ATTACK_KNOCKBACK);
+    public static final Attribute ATTACK_SPEED = new MinecraftAttribute("mc-attack_speed", ChatColor.AQUA, org.bukkit.attribute.Attribute.GENERIC_ATTACK_SPEED);
     public static final Attribute ARMOR = new MinecraftAttribute("mc-armor", ChatColor.GOLD, org.bukkit.attribute.Attribute.GENERIC_ARMOR);
     public static final Attribute ARMOR_TOUGHNESS = new MinecraftAttribute("mc-armor_toughness", ChatColor.DARK_GREEN, org.bukkit.attribute.Attribute.GENERIC_ARMOR_TOUGHNESS);
     public static final Attribute LUCK = new MinecraftAttribute("mc-luck", ChatColor.GREEN, org.bukkit.attribute.Attribute.GENERIC_MAX_HEALTH);
